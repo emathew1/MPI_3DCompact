483f3d10128d7fa10c653093f01386207736e3a3
emathew1 <emathew1@nd.edu>
2019-02-20 21:41:55 -0600

    Initial class roadmap for statistics

---
+++ src/Stats.hpp
@@ -0,0 +1,84 @@
+#ifndef STATS_HPP
+#define STATS_HPP
+
+#include "Options.hpp"
+#include "CurvilinearCSolver.hpp"
+
+class Stats{
+
+    public:
+
+	CurvilinearCSolver *cs;
+	Options *opt;
+
+	double prev_time;
+
+	bool velocityStats;
+	double velocityStatsWeight;
+	string velocityStatsFilename;
+	double *UAVG, *VAVG, *WAVG;
+	double *URMS, *VRMS, *WRMS;
+	double *UVREY, *UWREY, *VWREY;
+
+	bool thermoStats;
+	double thermoStatsWeight;
+	string thermoStatsFilename;
+	double *RHOAVG, *RHORMS;
+	double *PAVG, *PRMS;
+	double *TAVG, *TRMS;
+
+	Options::StatsAvgType statsAvgType; 
+
+
+	Stats(CurvilinearCSolver *cs, Options *opt){
+
+	    this->cs  = cs;
+	    this->opt = opt;
+ 
+	    //get the flags for the statistics
+	    velocityStats = opt->velocityStats;
+	    thermoStats   = opt->thermoStats;
+
+	    //If from restart file
+	    if(opt->velocityStats){
+
+		//allocate the arrays...
+
+	        if(opt->velocityStatsFromRestart){
+		     //read in the weight and the stat fields from the files
+
+	        }else{
+		
+		     //reset the weights = 0.0
+		     //initialize the fields
+		}
+	    }	
+
+
+	    if(opt->thermoStats){
+
+		//allocate the arrays
+
+		//allocate the arrays...
+		if(opt->thermoStatsFromRestart){
+		    //read in the weight and the stat fields from the files
+
+		}else{
+		
+		     //reset the weights = 0.0
+		     //initialize the fields
+		}
+
+	    }
+
+
+	    
+	    if(opt->restartStats){
+		weight = 0.0;
+	    }
+	}
+
+
+};
+
+#endif
