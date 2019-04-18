14d1ac1beea47c8d302fb4bb18b386d976d493b7
emathew1 <emathew1@alumni.nd.edu>
2019-04-18 11:34:19 -0500

    Working Linear Interpolation & Working Wall boundary filter and deriv. schemes
    Should have a linear interpolation method that works now, also pushing the boundary filtering and derivative schemes for Pade 6 and Compact 10 that appear to be stable, at least in the cylinder test case

--- src/CurvilinearInterpolator.hpp
+++ src/CurvilinearInterpolator.hpp
@@ -90,16 +90,15 @@ class CurvilinearInterpolator{
 	//For each of local found points
 	for(int ii = 0; ii < localPointFoundCount; ii++){
 
-		
-	    //To confuse things even more, box is oriented differently
-	    //point 1 ->  1  1  1, box_p index -> 7
-	    //point 2 -> -1  1  1, box_p index -> 3
-	    //point 3 -> -1  1 -1, box_p index -> 2
-	    //point 4 ->  1  1 -1, box_p index -> 6
-	    //point 5 ->  1 -1  1, box_p index -> 5
-	    //point 6 -> -1 -1  1, box_p index -> 1
-	    //point 7 -> -1 -1 -1, box_p index -> 0
-	    //point 8 ->  1 -1 -1, box_p index -> 4
+	    //Going to try this one...
+	    //point 1 ->  0  0  0, box_p index -> 0
+	    //point 2 ->  1  0  0, box_p index -> 4
+	    //point 3 ->  0  1  0, box_p index -> 2
+	    //point 4 ->  1  1  0, box_p index -> 6
+	    //point 5 ->  0  0  1, box_p index -> 1
+	    //point 6 ->  1  0  1, box_p index -> 5
+	    //point 7 ->  0  1  1, box_p index -> 3
+	    //point 8 ->  1  1  1, box_p index -> 7
 
 	    //back out the ip, jp, kp based off index
 	    int icv = icvList[ii];
@@ -117,6 +116,7 @@ class CurvilinearInterpolator{
 
 	    //Just implement inverse distance weighting for now since we're just using it for visualization
 	    
+	   /* INVERSE DISTANCE WEIGHTING...
 	    //Get the distance from the point to the corners of the box
 	    double d2[8], p[3];
 	    p[0] = pointListX[ii];
@@ -128,7 +128,6 @@ class CurvilinearInterpolator{
 		    d2[i] += (box_p[i][j]-p[j])*(box_p[i][j]-p[j]); 
 		}
 	    }
-	   /* INVERSE DISTANCE WEIGHTING...
 	    double total_weight = 0.0;
 	    for(int i = 0; i < 8; i++){
 		total_weight += 1.0/d2[i];
@@ -143,40 +142,35 @@ class CurvilinearInterpolator{
 	    //reorder to make things easier to program
 	    double xp[8][3];
 	    FOR_I3{
-		xp[0][i] = box_p[7][i];
-		xp[1][i] = box_p[3][i];
+		xp[0][i] = box_p[0][i];
+		xp[1][i] = box_p[4][i];
 		xp[2][i] = box_p[2][i];
 		xp[3][i] = box_p[6][i];
-		xp[4][i] = box_p[5][i];
-		xp[5][i] = box_p[1][i];
-		xp[6][i] = box_p[0][i];
-		xp[7][i] = box_p[4][i];
+		xp[4][i] = box_p[1][i];
+		xp[5][i] = box_p[5][i];
+		xp[6][i] = box_p[3][i];
+		xp[7][i] = box_p[7][i];
 	    }
 
 
 	    double f[8][3];
+	    double xp_finding[3] = {pointListX[ii], pointListY[ii], pointListZ[ii]};
 
 	    //Get the f values for x, y, & z
 	    FOR_I3{
-		f[0][i] =  xp[0][i] + xp[1][i] + xp[2][i] + xp[3][i] + xp[4][i] + xp[5][i] + xp[6][i] + xp[7][i];
-		f[1][i] =  xp[0][i] - xp[1][i] - xp[2][i] + xp[3][i] + xp[4][i] - xp[5][i] - xp[6][i] + xp[7][i];
-		f[2][i] =  xp[0][i] + xp[1][i] + xp[2][i] + xp[3][i] - xp[4][i] - xp[5][i] - xp[6][i] - xp[7][i];
-		f[3][i] =  xp[0][i] + xp[1][i] - xp[2][i] - xp[3][i] + xp[4][i] + xp[5][i] - xp[6][i] - xp[7][i];
-		f[4][i] =  xp[0][i] - xp[1][i] - xp[2][i] + xp[3][i] - xp[4][i] + xp[5][i] + xp[6][i] - xp[7][i];
-		f[5][i] =  xp[0][i] + xp[1][i] - xp[2][i] - xp[3][i] - xp[4][i] - xp[5][i] + xp[6][i] + xp[7][i];
-		f[6][i] =  xp[0][i] - xp[1][i] + xp[2][i] - xp[3][i] + xp[4][i] - xp[5][i] + xp[6][i] - xp[7][i];
-		f[7][i] =  xp[0][i] - xp[1][i] + xp[2][i] - xp[3][i] - xp[4][i] + xp[5][i] - xp[6][i] + xp[7][i];
-	    }
-
-	    for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 3; j++){
-		    f[i][j] *= (1.0/8.0);
-		}
+		f[0][i] =  (xp[7][i] + xp[6][i] + xp[5][i] + xp[4][i] + xp[3][i] + xp[2][i] + xp[1][i] + xp[0][i])/8.0 - xp_finding[i];
+		f[1][i] =  (xp[7][i] - xp[6][i] + xp[5][i] - xp[4][i] + xp[3][i] - xp[2][i] + xp[1][i] - xp[0][i])/8.0;
+		f[2][i] =  (xp[7][i] + xp[6][i] - xp[5][i] - xp[4][i] + xp[3][i] + xp[2][i] - xp[1][i] - xp[0][i])/8.0;
+		f[3][i] =  (xp[7][i] + xp[6][i] + xp[5][i] + xp[4][i] - xp[3][i] - xp[2][i] - xp[1][i] - xp[0][i])/8.0;
+		f[4][i] =  (xp[7][i] - xp[6][i] - xp[5][i] + xp[4][i] + xp[3][i] - xp[2][i] - xp[1][i] + xp[0][i])/8.0;
+		f[5][i] =  (xp[7][i] - xp[6][i] + xp[5][i] - xp[4][i] - xp[3][i] + xp[2][i] - xp[1][i] + xp[0][i])/8.0;
+		f[6][i] =  (xp[7][i] + xp[6][i] - xp[5][i] - xp[4][i] - xp[3][i] - xp[2][i] + xp[1][i] + xp[0][i])/8.0;
+		f[7][i] =  (xp[7][i] - xp[6][i] - xp[5][i] + xp[4][i] - xp[3][i] + xp[2][i] + xp[1][i] - xp[0][i])/8.0;
 	    }
 
 	    bool done = false;
 	    int iter = 0;
-	    
+
 	    double e1 = 0.0, e2 = 0.0, e3 = 0.0; 
 	    while(!done){
 
@@ -184,29 +178,39 @@ class CurvilinearInterpolator{
 
 	        double error_tol = 1E-12;
 
-		double alpha = f[0][0] + f[1][0]*e1 + f[2][0]*e2 + f[3][0]*e3 + f[4][0]*e1*e2 + \
-			       f[5][0]*e2*e3 + f[6][0]*e1*e3 + f[7][0]*e1*e2*e3 - pointListX[ii]; 
-		double beta  = f[0][1] + f[1][1]*e1 + f[2][1]*e2 + f[3][1]*e3 + f[4][1]*e1*e2 + \
-			       f[5][1]*e2*e3 + f[6][1]*e1*e3 + f[7][1]*e1*e2*e3 - pointListY[ii]; 
-		double gamma = f[0][2] + f[1][2]*e1 + f[2][2]*e2 + f[3][2]*e3 + f[4][2]*e1*e2 + \
-			       f[5][2]*e2*e3 + f[6][2]*e1*e3 + f[7][2]*e1*e2*e3 - pointListZ[ii]; 
+		double ff = f[0][0] + f[1][0]*e1 + f[2][0]*e2 + f[3][0]*e3 + f[4][0]*e1*e2 + \
+			       f[5][0]*e1*e3 + f[6][0]*e2*e3 + f[7][0]*e1*e2*e3; 
+		double gg = f[0][1] + f[1][1]*e1 + f[2][1]*e2 + f[3][1]*e3 + f[4][1]*e1*e2 + \
+			       f[5][1]*e1*e3 + f[6][1]*e2*e3 + f[7][1]*e1*e2*e3; 
+		double hh = f[0][2] + f[1][2]*e1 + f[2][2]*e2 + f[3][2]*e3 + f[4][2]*e1*e2 + \
+			       f[5][2]*e1*e3 + f[6][2]*e2*e3 + f[7][2]*e1*e2*e3; 
 
 
 	   	double J[3][3];
 
-		J[0][0] = f[1][0] + f[4][0]*e2 + f[6][0]*e3 + f[7][0]*e2*e3;
-		J[1][0] = f[1][1] + f[4][1]*e2 + f[6][1]*e3 + f[7][1]*e2*e3;
-		J[2][0] = f[1][2] + f[4][2]*e2 + f[6][2]*e3 + f[7][2]*e2*e3;
+		double A00 = f[1][0] + f[4][0]*e2 + f[5][0]*e3 + f[7][0]*e2*e3;
+	 	double A10 = f[1][1] + f[4][1]*e2 + f[5][1]*e3 + f[7][1]*e2*e3;
+		double A20 = f[1][2] + f[4][2]*e2 + f[5][2]*e3 + f[7][2]*e2*e3;
 
 
-		J[0][1] = f[2][0] + f[4][0]*e1 + f[5][0]*e3 + f[7][0]*e1*e3;
-		J[1][1] = f[2][1] + f[4][1]*e1 + f[5][1]*e3 + f[7][1]*e1*e3;
-		J[2][1] = f[2][2] + f[4][2]*e1 + f[5][2]*e3 + f[7][2]*e1*e3;
+		double A01 = f[2][0] + f[4][0]*e1 + f[6][0]*e3 + f[7][0]*e1*e3;
+		double A11 = f[2][1] + f[4][1]*e1 + f[6][1]*e3 + f[7][1]*e1*e3;
+		double A21 = f[2][2] + f[4][2]*e1 + f[6][2]*e3 + f[7][2]*e1*e3;
 
 
-		J[0][2] = f[3][0] + f[5][0]*e2 + f[6][0]*e1 + f[7][0]*e1*e2;
-		J[1][2] = f[3][1] + f[5][1]*e2 + f[6][1]*e1 + f[7][1]*e1*e2;
-		J[2][2] = f[3][2] + f[5][2]*e2 + f[6][2]*e1 + f[7][2]*e1*e2;
+		double A02 = f[3][0] + f[5][0]*e1 + f[6][0]*e2 + f[7][0]*e1*e2;
+		double A12 = f[3][1] + f[5][1]*e1 + f[6][1]*e2 + f[7][1]*e1*e2;
+		double A22 = f[3][2] + f[5][2]*e1 + f[6][2]*e2 + f[7][2]*e1*e2;
+
+		J[0][0] = A00;
+		J[0][1] = A01;
+		J[0][2] = A02;
+		J[1][0] = A10;
+		J[1][1] = A11;
+		J[1][2] = A12;
+		J[2][0] = A20;
+		J[2][1] = A21;
+		J[2][2] = A22;
 
 		double Jinv[3][3];
 		
@@ -221,7 +225,9 @@ class CurvilinearInterpolator{
 		H = -(J[0][0]*J[1][2] - J[0][2]*J[1][0]);
 		I =  (J[0][0]*J[1][1] - J[0][1]*J[1][0]);
 
-		double Jdet = J[0][0]*A + J[1][1]*B + J[2][2]*C;
+		double Jdet = J[0][0]*((J[1][1]*J[2][2]) - (J[2][1]*J[1][2])) -
+			      J[0][1]*((J[1][0]*J[2][2]) - (J[2][0]*J[0][2])) +
+			      J[0][2]*((J[1][0]*J[2][1]) - (J[2][0]*J[1][1]));
 
 		
 		Jinv[0][0] = A/Jdet;
@@ -235,11 +241,11 @@ class CurvilinearInterpolator{
 		Jinv[2][2] = I/Jdet;
 
 		double delta[3];
-		FOR_I3	delta[i] = alpha*Jinv[i][0] + beta*Jinv[i][1] + gamma*Jinv[i][2];
+		FOR_I3	delta[i] = -ff*Jinv[i][0] + -gg*Jinv[i][1] + -hh*Jinv[i][2];
 			
-		double e1_new = e1 - delta[0];
-		double e2_new = e2 - delta[1];
-		double e3_new = e3 - delta[2];
+		double e1_new = e1 + delta[0];
+		double e2_new = e2 + delta[1];
+		double e3_new = e3 + delta[2];
  
 		double eps[3];
 		eps[0] = fabs(e1_new-e1); 
@@ -250,6 +256,11 @@ class CurvilinearInterpolator{
 		    done = true;
 		}
 
+		if(iter > 20){
+		    cout << " > Could not converge on iterations to get trilinear interpolant! " << endl;
+		    MPI_Abort(MPI_COMM_WORLD, -10);
+		}	
+	
 		e1 = e1_new;
 		e2 = e2_new;
 		e3 = e3_new;
